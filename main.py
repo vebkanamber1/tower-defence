09a0c6c099d352d06e208c8d060b48ae9091a558
krononpark <57452349+krononpark@users.noreply.github.com>
2025-04-12 11:26:14 +0300

    проблема при бэк старт не сделан скор нужно добавлять новые картинки

--- main.py
+++ main.py
@@ -12,10 +12,11 @@ FPS = 60
 sc = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 from load import *
-font = pygame.font.SysFont('Aria', 40 )
-lvl ='menu'
+
+font = pygame.font.SysFont('Aria', 40)
+lvl = 'menu'
 lvl_game = 1
-money = 500000
+money = 500
 score = 0
 
 
@@ -25,11 +26,20 @@ def startMenu():
     button_group.update()
     pygame.display.update()
 
+
 def changelvl():
     sc.fill('grey')
+
     button2_group.draw(sc)
     button2_group.update()
     pygame.display.update()
+
+
+# def score():
+#     sc.fill('grey')
+#     button_group.draw(sc)
+#     button_group.update()
+#     pygame.display.update()
 
 
 class Button(pygame.sprite.Sprite):
@@ -41,25 +51,47 @@ class Button(pygame.sprite.Sprite):
         self.text = text
 
         self.next_lvl = next_lvl
+        self.timer_click = 0
 
     def update(self):
-        global lvl
-
+        global lvl, money
+        self.timer_click += 1
         text_render = font.render(self.text, True, 'white')
         sc.blit(text_render, (self.rect.x + 80, self.rect.y + 5))
 
         click = pygame.mouse.get_pos()
-        if pygame.mouse.get_pressed()[0]:
+        if pygame.mouse.get_pressed()[0] and self.timer_click / FPS > 0.3:
             if self.rect.left < click[0] < self.rect.right and self.rect.top < click[1] < self.rect.bottom:
                 lvl = self.next_lvl
             if lvl == 'choise':
-                button_back = Button(button_image, (540, 270), 'back', 'back')
+                button_back = Button(button_image, (540, 350), 'menu', 'back')
                 button2_group.add(button_back)
 
-                button_lvl1 = Button(button_image, (540, 100), 'lvl_1', 'lvl 1')
+                button_lvl1 = Button(button_image, (540, 100), 'lvl 1', 'lvl 1')
                 button2_group.add(button_lvl1)
-            if lvl == 'game_lvl':
+
+                button_lvl2 = Button(button_image, (540, 180), 'lvl 2', 'lvl 2')
+                button2_group.add(button_lvl2)
+
+                button_lvl3 = Button(button_image, (540, 270), 'lvl 3', 'lvl 3')
+                button2_group.add(button_lvl3)
+
+            if lvl == 'lvl 1':
+                money = 500
                 restart()
+                drawmaps('game_lvl1')
+
+            if lvl == 'lvl 2':
+                money = 400
+                restart()
+
+                drawmaps('game_lvl2')
+
+            if lvl == 'lvl 3':
+                money = 500
+                restart()
+
+                drawmaps('game_lvl3')
             if lvl == 'end':
                 exit()
 
@@ -80,6 +112,7 @@ class Tower_bush(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = pos[0]
         self.rect.y = pos[1]
+
     def update(self):
         global money
         if tower_shop_1.buy == True and pygame.mouse.get_pressed()[0]:
@@ -90,9 +123,6 @@ class Tower_bush(pygame.sprite.Sprite):
                 tower_shots_group.add(tower_shots)
                 tower_shop_1.buy = False
                 money -= 100
-
-
-
 
 
 class Grass(pygame.sprite.Sprite):
@@ -154,11 +184,17 @@ class Enemy(pygame.sprite.Sprite):
         self.dir = 'right'
         self.speedx = 2
         self.speedy = 0
-        self.hp = 100
-
+        self.max_hp = 100
+        self.hp = self.max_hp
+        if lvl =='lvl 2':
+            self.max_hp = 120
+            self.hp = self.max_hp
+        if lvl =='lvl 3':
+            self.max_hp = 180
+            self.hp = self.max_hp
 
     def update(self):
-        global money, score
+        global money, score,lvl
         self.draw_stats_enemy()
         self.rect.x += self.speedx
         self.rect.y += self.speedy
@@ -181,26 +217,31 @@ class Enemy(pygame.sprite.Sprite):
 
         if pygame.sprite.spritecollide(self, edit_dir_group, False):
             tile = pygame.sprite.spritecollide(self, edit_dir_group, False)[0]
-            if abs(self.rect.centerx - tile.rect.centerx) <= 5 and abs(self.rect.centery - tile.rect.centery) <=5:
+            if abs(self.rect.centerx - tile.rect.centerx) <= 5 and abs(self.rect.centery - tile.rect.centery) <= 5:
                 self.dir = tile.dir
         if pygame.sprite.spritecollide(self, bullet_group, False):
             bullet = pygame.sprite.spritecollide(self, bullet_group, False)[0]
             self.hp -= bullet.damage
             bullet.kill()
-        if self.hp <=0:
+        if self.hp <= 0:
             money += 20
             score += 10
             self.kill()
+        if lvl =='lvl 2':
+            self.max_hp = 120
+
+
+        if lvl =='lvl 3':
+            self.max_hp= 180
 
 
 
 
     def draw_stats_enemy(self):
-        if self.hp <= 99:
-            width_hp = 96 * (self.hp / 100)
+        if self.hp < self.max_hp:
+            width_hp = 96 * (self.hp / self.max_hp)
             pygame.draw.rect(sc, 'black', (self.rect.x - 30, self.rect.y - 52, 100, 20), 2)
             pygame.draw.rect(sc, 'green', (self.rect.x - 27, self.rect.y - 50, width_hp, 15))
-
 
 
 class Edit_dir_tile(pygame.sprite.Sprite):
@@ -220,13 +261,31 @@ class Spawner():
     def update(self):
         self.spawn()
 
-
     def spawn(self):
-        self.timer_spawn += 1
-        if self.timer_spawn / FPS > 1:
-            enemy = Enemy(enemy_image, (0,680))
-            enemy_group.add(enemy)
-            self.timer_spawn = 0
+        global lvl
+        if lvl =='lvl 1':
+            self.timer_spawn += 1
+            if self.timer_spawn / FPS > 1:
+                enemy = Enemy(enemy_image, (0, 680))
+                enemy_group.add(enemy)
+                self.timer_spawn = 0
+        elif lvl == 'lvl 2':
+            self.timer_spawn += 1
+            if self.timer_spawn / FPS > 1:
+                enemy = Enemy(enemy_image, (0, 360))
+                enemy_group.add(enemy)
+                self.timer_spawn = 0
+        elif lvl =='lvl 3':
+            self.timer_spawn += 1
+            if self.timer_spawn / FPS > 1.8:
+                enemy = Enemy(enemy_image, (0, 360))
+                enemy_group.add(enemy)
+                self.timer_spawn = 0
+                enemy = Enemy(enemy_image, (200, 0))
+                enemy_group.add(enemy)
+                enemy.dir = 'bottom'
+                self.timer_spawn = 0
+
 
 
 
@@ -243,21 +302,15 @@ class Tower_afk(pygame.sprite.Sprite):
 
     def update(self):
         pos = pygame.mouse.get_pos()
-        if money <= 100:
+        if money < 100:
             self.image = tower_1_of
         elif pygame.mouse.get_pressed()[0] and money >= 100:
 
-            if self.rect.left < pos[0] < self.rect.right\
+            if self.rect.left < pos[0] < self.rect.right \
                     and self.rect.top < pos[1] < self.rect.bottom:
-
                 self.buy = True
         if self.buy:
             sc.blit(tower_1_on, pos)
-
-
-
-
-
 
 
 class Tower_shots(pygame.sprite.Sprite):
@@ -277,26 +330,20 @@ class Tower_shots(pygame.sprite.Sprite):
     def update(self):
         self.upgrades()
         self.shot()
-        print(self.enemy)
+
 
         if self.enemy is None:
             for enemy in enemy_group:
                 if ((self.rect.centerx - enemy.rect.centerx) ** 2 + (
-                        self.rect.centerx - enemy.rect.centery) ** 2) ** 0.5 <500:
+                        self.rect.centerx - enemy.rect.centery) ** 2) ** 0.5 < 500:
                     self.enemy = enemy
                     break
         if self.enemy not in enemy_group:
             self.enemy = None
 
-        if len(enemy_group) == 0:
-            lvl_game += 1
-            restart()
-            drawMaps(str(lvl_game) + '.txt')
-
     def upgrades(self):
         global money
         if self.lvl == 1 and money >= 200:
-
             self.upgrade = True
 
         if self.upgrade == True:
@@ -304,47 +351,44 @@ class Tower_shots(pygame.sprite.Sprite):
 
         if self.upgrade == True and pygame.mouse.get_pressed()[0]:
             pos = pygame.mouse.get_pos()
-            if self.rect.right< pos[0] < self.rect.right+50\
-                     and self.rect.bottom < pos[1] < self.rect.bottom+45:
+            if self.rect.right < pos[0] < self.rect.right + 50 \
+                    and self.rect.bottom < pos[1] < self.rect.bottom + 45:
+                money -=200
                 self.lvl = 2
                 self.damage = 50
                 self.image = tower_1_1_image[0]
                 Bullet.speed = 15
                 self.upgrade = False
 
-
-
     def shot(self):
         self.timer_shot += 1
-        if self.enemy != None and self.timer_shot/ FPS > 1:
+        if self.enemy != None and self.timer_shot / FPS > 1:
             x_1 = self.rect.centerx
             y_1 = self.rect.top
             x_2 = self.enemy.rect.centerx
             y_2 = self.enemy.rect.centery
-            bullet = Bullet(self.current_bullet_image, (x_1, y_1, x_2, y_2), self.damage, 5*self.lvl)
+            bullet = Bullet(self.current_bullet_image, (x_1, y_1, x_2, y_2), self.damage, 5 * self.lvl)
             bullet_group.add(bullet)
             self.timer_shot = 0
 
+
 class Bullet(pygame.sprite.Sprite):
-    def __init__(self,image,pos,damage, speed = 5):
+    def __init__(self, image, pos, damage, speed=5):
         pygame.sprite.Sprite.__init__(self)
         self.image = image
-        self.rect =self.image.get_rect()
+        self.rect = self.image.get_rect()
         self.speed = speed
         self.damage = damage
-        self.start_pos = pygame.math.Vector2(pos[0],pos[1])
+        self.start_pos = pygame.math.Vector2(pos[0], pos[1])
         self.end_pos = pygame.math.Vector2(pos[2], pos[3])
-        self.velocity = (self.end_pos - self.start_pos).normalize() *self.speed
+        self.velocity = (self.end_pos - self.start_pos).normalize() * self.speed
         self.rect.center = self.start_pos
 
     def update(self):
-
         self.rect.center += self.velocity
 
 
-
-
-def game_lvl():
+def game_lvl1():
     global pygame
     sc.fill('grey')
     grass_group.update()
@@ -361,17 +405,85 @@ def game_lvl():
     right_group.draw(sc)
     left_group.update()
     left_group.draw(sc)
-
-
-
     edit_dir_group.update()
     edit_dir_group.draw(sc)
     sc.blit(panel_image, (0, 720))
-    # sc.blit(upgrade_image, (0, 500))
     money_counter = font.render(f'Деньги: {money}', True, 'black')
     score_counter = font.render(f'Score: {score}', True, 'black')
     sc.blit(money_counter, (40, 40))
-    sc.blit(score_counter, (40,75))
+    sc.blit(score_counter, (40, 75))
+    tower_afk_group.update()
+    tower_afk_group.draw(sc)
+    tower_shots_group.update()
+    tower_shots_group.draw(sc)
+    spawner.update()
+    enemy_group.update()
+    enemy_group.draw(sc)
+    bullet_group.update()
+    bullet_group.draw(sc)
+    pygame.display.update()
+
+
+def game_lvl2():
+    global pygame
+    sc.fill('grey')
+    grass_group.update()
+    grass_group.draw(sc)
+    tower_bush_group.update()
+    tower_bush_group.draw(sc)
+    bush_group.update()
+    bush_group.draw(sc)
+    up_group.update()
+    up_group.draw(sc)
+    bottom_group.update()
+    bottom_group.draw(sc)
+    right_group.update()
+    right_group.draw(sc)
+    left_group.update()
+    left_group.draw(sc)
+    edit_dir_group.update()
+    edit_dir_group.draw(sc)
+    sc.blit(panel_image, (0, 720))
+    money_counter = font.render(f'Деньги: {money}', True, 'black')
+    score_counter = font.render(f'Score: {score}', True, 'black')
+    sc.blit(money_counter, (40, 40))
+    sc.blit(score_counter, (40, 75))
+    tower_afk_group.update()
+    tower_afk_group.draw(sc)
+    tower_shots_group.update()
+    tower_shots_group.draw(sc)
+    spawner.update()
+    enemy_group.update()
+    enemy_group.draw(sc)
+    bullet_group.update()
+    bullet_group.draw(sc)
+    pygame.display.update()
+
+
+def game_lvl3():
+    global pygame
+    sc.fill('grey')
+    grass_group.update()
+    grass_group.draw(sc)
+    tower_bush_group.update()
+    tower_bush_group.draw(sc)
+    bush_group.update()
+    bush_group.draw(sc)
+    up_group.update()
+    up_group.draw(sc)
+    bottom_group.update()
+    bottom_group.draw(sc)
+    right_group.update()
+    right_group.draw(sc)
+    left_group.update()
+    left_group.draw(sc)
+    edit_dir_group.update()
+    edit_dir_group.draw(sc)
+    sc.blit(panel_image, (0, 720))
+    money_counter = font.render(f'Деньги: {money}', True, 'black')
+    score_counter = font.render(f'Score: {score}', True, 'black')
+    sc.blit(money_counter, (40, 40))
+    sc.blit(score_counter, (40, 75))
     tower_afk_group.update()
     tower_afk_group.draw(sc)
     tower_shots_group.update()
@@ -385,10 +497,8 @@ def game_lvl():
 
 
 def restart():
-    global spawner,tower_shots_group,grass_group,tower_afk_group, tower_bush_group, bush_group, enemy_group, spawner_group, up_group, edit_dir_group, bottom_group, left_group, right_group
-    global tower_shop_1, bullet_group,button_group,button2_group
-
-
+    global spawner, tower_shots_group, grass_group, tower_afk_group, tower_bush_group, bush_group, enemy_group, spawner_group, up_group, edit_dir_group, bottom_group, left_group, right_group
+    global tower_shop_1, bullet_group, button_group, button2_group
 
     grass_group = pygame.sprite.Group()
     tower_bush_group = pygame.sprite.Group()
@@ -409,17 +519,18 @@ def restart():
     spawner = Spawner()
     bullet_group = pygame.sprite.Group()
     tower_shots_group = pygame.sprite.Group()
-    tower_shop_1 = Tower_afk(tower_1_on, (45,765))
+    tower_shop_1 = Tower_afk(tower_1_on, (45, 765))
     tower_afk_group.add(tower_shop_1)
 
     button_start = Button(button_image, (540, 100), 'choise', 'start')
     button_group.add(button_start)
 
-    button_score = Button(button_image, (540,180),'score', 'score')
+    button_score = Button(button_image, (540, 180), 'score', 'score')
     button_group.add(button_score)
 
     button_exit = Button(button_image, (540, 270), 'end', 'exit')
     button_group.add(button_exit)
+
 
 def drawmaps(namefile):
     global player
@@ -458,14 +569,7 @@ def drawmaps(namefile):
                     edit_dir_group.add(left)
 
 
-
-
-
 restart()
-
-
-
-drawmaps('game lvl')
 
 while True:
     for event in pygame.event.get():
@@ -474,16 +578,20 @@ while True:
             sys.exit()
 
     if lvl == 'game':
-        game_lvl()
+        game_lvl1()
     elif lvl == 'menu':
         startMenu()
     elif lvl == 'exit':
         pygame.quit()
-        game_lvl()
+        game_lvl1()
     elif lvl == 'choise':
         changelvl()
-    elif lvl =='back':
+    elif lvl == 'back':
         startMenu()
-    elif lvl =='lvl_1':
-        game_lvl()
+    elif lvl == 'lvl 1':
+        game_lvl1()
+    elif lvl == 'lvl 2':
+        game_lvl2()
+    elif lvl == 'lvl 3':
+        game_lvl3()
     clock.tick(FPS)
